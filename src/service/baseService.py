5111e63fa16072848dc0a37cc604b5f6babba2d4
Zhengkai Xie <xiezhengkai199982@gmail.com>
2022-11-09 17:05:37 -0500

    base service file

---
+++ src/service/baseService.py
@@ -0,0 +1,24 @@
+import pymysql
+
+import os
+
+
+class BaseService:
+
+    def __int__(self):
+        pass
+
+    @staticmethod
+    def _get_connection():
+        usr = os.environ.get("DBUSER")
+        pw = os.environ.get("DBPW")
+        h = os.environ.get("DBHOST")
+
+        conn = pymysql.connect(
+            user=usr,
+            password=pw,
+            host=h,
+            cursorclass=pymysql.cursors.DictCursor,
+            autocommit=True
+        )
+        return conn
